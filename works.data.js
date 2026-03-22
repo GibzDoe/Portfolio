d06e6ed108dd7d633adaad9a5250b8d4e6c73e60
Gibongz <gibzdev200@gmail.com>
2026-03-22 23:42:29 +0800

    Initial commit

---
+++ works.data.js
@@ -0,0 +1,20 @@
+window.WORKS = [
+  {
+    "title": "Test Project",
+    "image": "Background.jpg",
+    "description": "A sample project used to demonstrate dynamic loading from JSON.",
+    "tags": ["Demo", "Godot", "Lua"]
+  },
+  {
+    "title": "Test",
+    "image": "https://wallpapercave.com/wp/wp16233910.webp",
+    "description": "A sample project used to demonstrate dynamic loading from JSON.",
+    "tags": ["Demo", "Godot"]
+  },
+  {
+    "title": "Test",
+    "image": "https://wallpapercave.com/wp/wp16233910.webp",
+    "description": "A sample project used to demonstrate dynamic loading from JSON.",
+    "tags": ["Demo", "Godot"]
+  }
+];
